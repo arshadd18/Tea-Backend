7c2eb22446f613e64ea695746a831033eb71bb77
arshadd18 <rshadshaik18@gmail.com>
2026-07-16 19:24:08 +0530

    add utils code

--- src/index.js
+++ src/index.js
@@ -6,37 +6,18 @@ dotenv.config({
     path:'./env'
 })
 
-connectDB();
- 
 
+connectDB()
+.then(()=>{
 
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is listening at port ${process.env.PORT}`);
 
+    })
 
+})
+.catch((err)=>{
 
+    console.log("MONGODB connection failed");
 
-/*
-import express from "express";
-
-const app=express();
-
-
-(async()=>{
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-
-        app.on("error",(error)=>{
-            console.log("ERROR:",error);
-            throw error
-        })
-        app.listen(process.env.PORT,()=>{
-            console.log(`App is listening to ${process.env.PORT} `);
-
-        });
-
-    }
-    catch(error){
-        console.log("ERROR:",error)
-        throw err
-    }
-})() 
-*/
+})
